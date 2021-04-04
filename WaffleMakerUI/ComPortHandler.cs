baf1ab38bccbd643cf034925bb5d7fae92315cb9
Assem Ibrahim Younis <lordsomy@hotmail.com>
2021-04-04 18:01:49 +0200

    Added ComPortHandler

---
+++ WaffleMakerUI/ComPortHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaffleMakerUI
+{
+	class ComPortHandler
+	{
+		bool CheckPortName(string portName)
+		{
+			string[] ports = SerialPort.GetPortNames();
+			for (int i = 0; i < ports.Length; i++)
+			{
+				if (ports[i].Contains(portName))
+					return true;
+			}
+
+			return false;
+		}
+	}
+}
