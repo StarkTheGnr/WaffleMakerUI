8bcf82d3a7c611fcfccb30ac5028b65ccc7bdd99
Assem Ibrahim Younis <lordsomy@hotmail.com>
2021-04-04 18:36:36 +0200

    Finished ComPortHandler

--- WaffleMakerUI/ComPortHandler.cs
+++ WaffleMakerUI/ComPortHandler.cs
@@ -10,6 +10,48 @@ namespace WaffleMakerUI
 {
 	class ComPortHandler
 	{
+		readonly string writePort = "COM1";
+		readonly string readPort = "COM2";
+
+		SerialPort serialPort;
+
+		public void initializePort(string portName, int baudRate, Parity parity, int databits, StopBits stopbits, Handshake handshake)
+		{
+			serialPort = new SerialPort(portName, baudRate, parity, databits, stopbits);
+			serialPort.Handshake =  handshake;
+		}
+
+		public void Open()
+		{
+			if (!serialPort.IsOpen && CheckPortName(serialPort.PortName))
+				serialPort.Open();
+		}
+
+		public void Close()
+		{
+			if (serialPort.IsOpen)
+				serialPort.Close();
+		}
+
+		public void SendData(string data)
+		{
+			if (!serialPort.IsOpen)
+				return;
+
+			serialPort.WriteLine(data);
+		}
+
+		public async void ReadData(Action<string> callback)
+		{
+			if (!serialPort.IsOpen)
+				return;
+
+			string output = "";
+			await Task.Run(() => { output = serialPort.ReadLine(); });
+
+			callback(output);
+		}
+
 		bool CheckPortName(string portName)
 		{
 			string[] ports = SerialPort.GetPortNames();
